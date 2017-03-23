18d6dfe4c4fee38cf0eb366ce7dac76ca2888eb3
tinaaquino <tinaaquino@10.1.37.187>
2017-03-22 20:14:41 -0700

    added comments for if statements

--- src/Driver.java
+++ src/Driver.java
@@ -14,35 +14,42 @@ public class Driver
 		System.out.println(Arrays.toString(args));
 		
 		try {
+			// map without an index flag and without a path flag
 			if (!map.hasFlag("-index") && !map.hasFlag("-path")) {
-				System.out.println("No path and index flag input");
+				System.out.println("No path and index flag input.");
 			}
-		
+			
+			// map without an index flag and with a path flag
 			if (!map.hasFlag("-index") && map.hasFlag("-path")) {
 				if (map.hasValue("-path")) {
 					InvertedIndexBuilder.traverseDirectory(Paths.get(map.getValue("-path")), index);
 				}
 			}
 		
+			// map with an index and without a path flag
 			if (map.hasFlag("-index") && !map.hasFlag("-path")) {
 				if (map.hasValue("-index")) {
 					InvertedIndexBuilder.traverseDirectory(Paths.get(map.getValue("-path")), index);
 				}
 				index.asJSON(Paths.get(defaultValue));
-				System.out.println("contains just index flag");
+				System.out.println("Contains an index flag.");
 			}
-		
-			if (map.hasFlag("-index") && map.hasFlag("-path")) {	
+			
+			// map with an index flag and a path flag
+			if (map.hasFlag("-index") && map.hasFlag("-path")) {
+				// map with an index value and a path value
 				if (map.hasValue("-index") && map.hasValue("-path")) {
 					InvertedIndexBuilder.traverseDirectory(Paths.get(map.getValue("-path")), index);
 					index.asJSON(Paths.get(map.getValue("-index")));		
 				}
-			
+				
+				// map with a path value and without an index value
 				if (map.hasValue("-path") && !map.hasValue("-index"))  {
 					InvertedIndexBuilder.traverseDirectory(Paths.get(map.getValue("-path")), index);
 					index.asJSON(Paths.get(defaultValue));
 				}
-			
+				
+				// map without a path value and with an index value
 				if (!map.hasValue("-path") && map.hasValue("-index")) {
 					InvertedIndexBuilder.traverseDirectory(Paths.get(map.getValue("-path")), index);
 				}
