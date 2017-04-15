3fb842bc7801ab7106e0780127c39b576a28c2dc
tinaaquino <tinaaquino@10.232.103.209>
2017-04-14 23:54:47 -0700

    removed unused imports

--- src/QueryHelper.java
+++ src/QueryHelper.java
@@ -5,8 +5,6 @@ import java.nio.file.Files;
 import java.nio.file.Path;
 import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.HashMap;
-import java.util.LinkedHashMap;
 import java.util.TreeMap;
 
 /**
