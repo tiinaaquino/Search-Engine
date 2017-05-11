52e32f3393c74fbb0477f3ac223dec2eae906552
tinaaquino <tinaaquino@10.1.37.187>
2017-05-11 01:26:04 -0700

    copied methods from InvertedIndex & @Override

--- src/ThreadSafeInvertedIndex.java
+++ src/ThreadSafeInvertedIndex.java
@@ -1,6 +1,7 @@
 import java.io.IOException;
 import java.nio.file.Path;
 import java.util.ArrayList;
+import java.util.List;
 
 public class ThreadSafeInvertedIndex extends InvertedIndex{
 	
@@ -77,5 +78,81 @@ public class ThreadSafeInvertedIndex extends InvertedIndex{
 		}
 	}
 	
-	// TODO Override all the reads (toString, copy, contains, num, etc.)
+	@Override
+	public int numWords() {
+		lock.lockReadOnly();
+		try {
+			return super.numWords();
+		}
+		finally {
+			lock.unlockReadOnly();
+		}
+	}
+	
+	@Override
+	public int numLocations(String word) {
+		lock.lockReadOnly();
+		try {
+			return super.numLocations(word);
+		}
+		finally {
+			lock.unlockReadOnly();
+		}
+	}
+	
+	@Override
+	public List<String> copyWords() {
+		lock.lockReadOnly();
+		try {
+			return super.copyWords();
+		}
+		finally {
+			lock.unlockReadOnly();
+		}
+	}
+	
+	@Override
+	public boolean contains(String word) {
+		lock.lockReadOnly();
+		try {
+			return super.contains(word);
+		}
+		finally {
+			lock.unlockReadOnly();
+		}
+	}
+	
+	@Override
+	public boolean contains(String word, String location) {
+		lock.lockReadOnly();
+		try {
+			return super.contains(word, location);
+		}
+		finally {
+			lock.unlockReadOnly();
+		}	
+	}
+	
+	@Override
+	public boolean contains (String word, String location, int wordPosition) {
+		lock.lockReadOnly();
+		try {
+			return super.contains(word, location, wordPosition);
+		}
+		finally {
+			lock.unlockReadOnly();
+		}
+	}
+	
+	@Override
+	public String toString() {
+		lock.lockReadOnly();
+		try {
+			return super.toString();
+		}
+		finally {
+			lock.unlockReadOnly();
+		}
+	}
+	
 }
