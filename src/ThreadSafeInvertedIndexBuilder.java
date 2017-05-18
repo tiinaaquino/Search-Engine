ae203d207fdc70d96cbf72ddc858dfe8c0f45f8f
tinaaquino <tinaaquino@10.0.0.174>
2017-05-18 02:00:21 -0700

    removed unusued imports

--- src/ThreadSafeInvertedIndexBuilder.java
+++ src/ThreadSafeInvertedIndexBuilder.java
@@ -1,6 +1,4 @@
 import java.io.IOException;
-import java.net.MalformedURLException;
-import java.net.URL;
 import java.nio.file.DirectoryStream;
 import java.nio.file.Files;
 import java.nio.file.Path;
