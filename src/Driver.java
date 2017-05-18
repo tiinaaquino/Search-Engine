b2af7fb59aedc9bb481505fe90771ee442883ee5
tinaaquino <tinaaquino@10.0.0.174>
2017-05-18 01:57:41 -0700

    SEARCH TESTS COMPILE

--- src/Driver.java
+++ src/Driver.java
@@ -1,138 +1,3 @@
-//import java.io.IOException;
-//import java.net.MalformedURLException;
-//import java.net.URL;
-//import java.nio.file.Paths;
-//
-///**
-// * Parses command-line arguments into the index.
-// */
-//public class Driver 
-//{
-//	/**
-//	 * Builds the inverted index data structure
-//	 * 
-//	 * @param args
-//	 * 				arguments to be parsed into
-//	 * 				into the index
-//	 */
-//	public static void main(String[] args)
-//	{	
-//		ArgumentMap argMap = new ArgumentMap(args);
-//		InvertedIndex index = null;
-//		ThreadSafeInvertedIndexBuilder builder = null;
-//		QueryInterface query = null;
-//		WorkQueue worker = null;
-//		int threads;
-//		int limit;
-//		String results = argMap.getString("-results", "results.json");
-//		String output = argMap.getString("-index", "index.json");
-//		WebCrawler crawler = null;
-//		URL url;
-//		
-//		try {
-//			threads = Integer.parseInt(argMap.getValue("-threads"));
-//			if (threads <= 0) {
-//				threads = 5;
-//			}
-//		}
-//		catch (NumberFormatException e) {
-//			threads = 5;
-//		}
-//		
-//		try {
-//			limit = Integer.parseInt(argMap.getValue("-limit"));
-//			if (limit <= 50) {
-//				limit = 50;
-//			}
-//		}
-//		catch (NumberFormatException e) {
-//			limit = 50;
-//		}
-//		
-//		try {
-//			url = new URL(argMap.getValue("-url"));
-//		}
-//		catch (MalformedURLException e) {
-//			System.out.println("Invalid URL");
-//		}
-//		
-//		if (argMap.hasFlag("-threads") && argMap.hasValue("-threads")) {
-//			ThreadSafeInvertedIndex threadedIndex = new ThreadSafeInvertedIndex();
-//			worker = new WorkQueue(threads);
-//			builder = new ThreadSafeInvertedIndexBuilder(threadedIndex, worker);
-//			query = new ThreadSafeQueryHelper(threadedIndex, worker); 
-//			index = threadedIndex;
-//		}
-//		else {
-//			index = new InvertedIndex();
-//			query = new QueryHelper(index);
-//			
-//		}
-//		
-//		if (argMap.hasFlag("-url") && argMap.hasValue("-url")) {
-//			ThreadSafeInvertedIndex threadedIndex = new ThreadSafeInvertedIndex();
-//			worker = new WorkQueue(threads);
-//			index = threadedIndex;
-//			crawler = new WebCrawler(threadedIndex, worker);
-//			try {
-//				url = new URL(argMap.getValue("-url"));
-//				crawler.crawl(url, limit);
-//			}
-//			catch (MalformedURLException e) {
-//				System.out.println("Invalid URL");
-//			}
-//		}
-//		
-//		if (argMap.hasFlag("-path") && argMap.hasValue("-path")) {
-//			try {
-//				if (builder == null) {
-//					InvertedIndexBuilder.traverse(Paths.get(argMap.getValue("-path")), index);
-//				}
-//				else {
-//					builder.traverse(Paths.get(argMap.getValue("-path")), (ThreadSafeInvertedIndex) index);
-//				}
-//			}
-//			catch (IOException e) {
-//				System.out.println("Unable to build index from the path " + argMap.getString("-path"));
-//			}
-//		}
-//		
-//		if (argMap.hasFlag("-query") && argMap.hasValue("-query")) {
-//			try {
-//				query.parse(Paths.get(argMap.getValue("-query")), argMap.hasFlag("-exact"));
-//			}
-//			catch (IOException e) {
-//				System.out.println("Unable to read query");
-//			}
-//		}
-//		
-//		if (argMap.hasFlag("-index")) {
-//			try {
-//				index.asJSON(Paths.get(output));
-//			}
-//			catch (IOException e) {
-//				System.out.println("Unable to write index from " + output);
-//			}
-//		}
-//		
-//		if (argMap.hasFlag("-results")) {
-//			try {
-//				System.out.println(1);
-//				query.asJSON(Paths.get(results));
-//				System.out.println(1);
-//			}
-//			catch (IOException e) {
-//				System.out.println("Unable to write results from " + results);
-//			}
-//		}
-//		
-//		if (worker != null) {
-//			worker.shutdown();
-//		}
-//		
-//	}	
-//}
-
 import java.io.IOException;
 import java.net.MalformedURLException;
 import java.net.URL;
@@ -148,20 +13,20 @@ public class Driver
 	 */
 	public static void main(String[] args)
 	{
-		ArgumentMap argument = new ArgumentMap(args);
-		InvertedIndex invertedIndex = null;
+		ArgumentMap argMap = new ArgumentMap(args);
+		InvertedIndex index = null;
 		QueryInterface queryHelper = null;
 		WorkQueue queue = null;
-		WebCrawler crawler = null;
 		int threads;
-		String results = argument.getString("-results", "results.json");
-		String output = argument.getString("-index", "index.json");
+		String results = argMap.getString("-results", "results.json");
+		String output = argMap.getString("-index", "index.json");
 		
+		WebCrawler crawler = null;
 		int total;
 		URL url = null;
 		
 		try  {
-			threads = Integer.parseInt(argument.getValue("-threads"));
+			threads = Integer.parseInt(argMap.getValue("-threads"));
 			
 			if (threads <= 0) {
 				threads = 5;
@@ -172,7 +37,7 @@ public class Driver
 		}
 		
 		try {
-			total = Integer.parseInt(argument.getValue("-limit"));
+			total = Integer.parseInt(argMap.getValue("-limit"));
 			
 			if (total <= 0) {
 				total = 50;
@@ -184,56 +49,58 @@ public class Driver
 		}
 		
 		try {
-			url = new URL(argument.getValue("-url"));
+			url = new URL(argMap.getValue("-url"));
 		} 
 		
 		catch (MalformedURLException e1)  {
-			System.out.println("invalid url");
+			System.out.println("Invalid URL");
 		}
 		
 		
-		if (argument.hasFlag("-threads") && argument.hasValue("-threads")) {
+		if (argMap.hasFlag("-threads") && argMap.hasValue("-threads")) {
 			queue = new WorkQueue(threads);
 			ThreadSafeInvertedIndex threadSafeIndex = new ThreadSafeInvertedIndex();
-			invertedIndex = threadSafeIndex;
+			index = threadSafeIndex;
 			queryHelper = new ThreadSafeQueryHelper(threadSafeIndex, queue);
 						
-			if (argument.hasFlag("-path") && argument.hasValue("-path")) {
+			if (argMap.hasFlag("-path") && argMap.hasValue("-path")) {
 				ThreadSafeInvertedIndexBuilder builder = new ThreadSafeInvertedIndexBuilder(threadSafeIndex, queue);
 				try  {
-					builder.traverse(Paths.get(argument.getValue("-path")), threadSafeIndex);
+					builder.traverse(Paths.get(argMap.getValue("-path")), threadSafeIndex);
 					
 				}
 				catch (IOException e) {
-					System.out.println("Unable to build index from the path" + argument.getString("-path"));
+					System.out.println("Unable to build index from the path" + argMap.getString("-path"));
 				}
 			}
 
 		}
 
 		else {
-			invertedIndex = new InvertedIndex();
-			queryHelper = new QueryHelper(invertedIndex);
+			index = new InvertedIndex();
+			queryHelper = new QueryHelper(index);
 			
-			if (argument.hasFlag("-path") && argument.hasValue("-path")) {	
+			if (argMap.hasFlag("-path") && argMap.hasValue("-path")) {	
 				try  {
-					InvertedIndexBuilder.traverse(Paths.get(argument.getValue("-path")), invertedIndex);
+					InvertedIndexBuilder.traverse(Paths.get(argMap.getValue("-path")), index);
 				}
 				
 				catch (IOException e) {
-					System.out.println("Unable to build index from the path" + argument.getString("-path"));
+					System.out.println("Unable to build index from the path" + argMap.getString("-path"));
 				}
 			}
 		}
 		
-		if (argument.hasFlag("-url") && argument.hasValue("-url")) {
+		if (argMap.hasFlag("-url") && argMap.hasValue("-url")) {
 			ThreadSafeInvertedIndex threadedIndex = new ThreadSafeInvertedIndex();
-			invertedIndex = threadedIndex;
+			index = threadedIndex;
 			queue = new WorkQueue();
 			crawler = new WebCrawler(threadedIndex, queue);
+			queryHelper = new ThreadSafeQueryHelper(threadedIndex, queue);
+
 
 			try {
-				URL urlFlag = new URL(argument.getValue("-url"));
+				URL urlFlag = new URL(argMap.getValue("-url"));
 				crawler.crawl(urlFlag, total);
 			} 
 			catch (MalformedURLException e) {
@@ -241,25 +108,25 @@ public class Driver
 			}
 		}
 		
-		if (argument.hasFlag("-query") && argument.hasValue("-query")) {
+		if (argMap.hasFlag("-query") && argMap.hasValue("-query")) {
 			try {
-				queryHelper.parse(Paths.get(argument.getValue("-query")), argument.hasFlag("-exact"));
+				queryHelper.parse(Paths.get(argMap.getValue("-query")), argMap.hasFlag("-exact"));
 			}
 			catch(IOException e) {
 				System.out.println("Unable to read query file");
 			}
 		}
 		
-		if (argument.hasFlag("-index")) {
+		if (argMap.hasFlag("-index")) {
 			try  {
-				invertedIndex.asJSON(Paths.get(output));
+				index.asJSON(Paths.get(output));
 			}
 			catch (IOException e) {
 				System.out.println("Unable to write index to the path" + output);
 			}
 		}
 
-		if (argument.hasFlag("-results")) {
+		if (argMap.hasFlag("-results")) {
 			try {
 				queryHelper.asJSON(Paths.get(results));
 			}
